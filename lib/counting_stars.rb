1544dbf20f7d61b74010a3216f7ff0f06e61c74d
Kerem Bozdas <krmbzds.github@gmail.com>
2015-02-27 11:46:49 +0200

    Add request and count methods

--- lib/counting_stars.rb
+++ lib/counting_stars.rb
@@ -1,5 +1,25 @@
-require "counting_stars/version"
+require 'uri'
+require 'open-uri'
+require 'nokogiri'
+require 'active_support/core_ext/object/to_query'
+require 'counting_stars/version'
 
 module CountingStars
-  # Your code goes here...
+
+  def self.count(query, site)
+    request "#{query} site:#{site}"
+  end
+
+  def self.request(query)
+    uri = URI::HTTPS.build(
+        :host  => 'encrypted.google.com',
+        :path  => '/search',
+        :query => { :q => query }.to_query
+    )
+
+    page = Nokogiri::HTML(open(uri))
+    page = page.css('div.slp').first.text
+    page.gsub(/[[:space:]]/, ' ').lstrip.split(' - ')
+  end
+
 end
