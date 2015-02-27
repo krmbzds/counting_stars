20a00f42262b7a40a8b105a9ec77a99e916ee881
Kerem Bozdas <krmbzds.github@gmail.com>
2015-02-27 11:44:59 +0200

    Add tests for module

--- spec/counting_stars_spec.rb
+++ spec/counting_stars_spec.rb
@@ -5,7 +5,41 @@ describe CountingStars do
     expect(CountingStars::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context 'booking.com' do
+    subject { CountingStars::count("Marriott's Grande Ocean", "booking.com") }
+
+    it 'returns a list of two items' do
+      subject.length.should eq(2)
+    end
+
+    it 'contains a rating' do
+      subject.first.should match(/Rating: \d.\d\/\d\d/) # Rating: 9.5/10
+    end
+
+    it 'contains x amount of reviews' do
+      subject.last.should match(/\d* reviews/) # "25 reviews"
+    end
   end
+
+  context 'tripadvisor.com' do
+    subject { CountingStars::count("Marriott's Grande Ocean", "tripadvisor.com") }
+
+    it 'returns a list of three items' do
+      subject.length.should eq(3)
+    end
+
+    it 'contains a rating' do
+      subject.first.should match(/Rating: \d.\d/) # Rating: 4.5
+    end
+
+    it 'contains x amount of reviews' do
+      subject[1].should match(/\d* reviews/) # "501 reviews"
+    end
+
+    it 'should contain a price range' do
+      subject.last.should match(/Price range: \$\$\$/) # Price range: $$$
+    end
+  end
+
+
 end
