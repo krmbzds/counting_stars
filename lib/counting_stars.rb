3acb166a630fd114e9f2602b5178f88593b28cd7
Kerem Bozdas <krmbzds.github@gmail.com>
2015-03-11 00:13:18 +0200

    Add shorthand methods

--- lib/counting_stars.rb
+++ lib/counting_stars.rb
@@ -6,6 +6,10 @@ require 'counting_stars/version'
 
 module CountingStars
 
+  INTEGER = /\d+/
+  DECIMAL = /\d+\.\d+/
+  DOLLARS = /\$+/
+
   def self.count(query, site)
     request "#{query} site:#{site}"
   end
@@ -20,6 +24,16 @@ module CountingStars
     page = Nokogiri::HTML(open(uri))
     page = page.css('div.slp').first.text
     page.gsub(/[[:space:]]/, ' ').lstrip.split(' - ')
+  end
+
+  def self.booking(query)
+    response = count(query, 'booking.com')
+    { rating: response[0][DECIMAL], reviews: response[1][INTEGER] }
+  end
+
+  def self.tripadvisor(query)
+    response = count(query, 'tripadvisor.com')
+    { rating: response[0][INTEGER], reviews: response[1][INTEGER], price_range: response[2][DOLLARS] }
   end
 
 end
